437539d48d05b02b765431153e0cbf10b16a287c
Quardin Lyttle <lyttleqd@gmail.com>
2026-04-23 02:41:28 -0400

    chore: remove obsolete simulation testbench binary and update rtl file list

--- rtl.f
+++ rtl.f
@@ -1,12 +1,10 @@
-# 1. Submodules
-# NOTE: lib/common-rtl submodule is not populated locally; resolve via the
-# HSU repo which is the canonical populated copy in this workspace.
--f ../../HSU/hash-sampler-unit/lib/common-rtl/rtl.f
+# 1. Submodules (i.e. -f lib/keccak-fips202-sv/rtl.f)
+-f lib/common-rtl/rtl.f
 
-# 2. Local Packages
+# 2. Local Packages (i.e., rtl/my_pkg.sv)
 rtl/core_ctrl_pkg.sv
 
-# 3. Local RTL
+# 3. Local RTL (i.e., rtl/transcoder_unit.sv)
 rtl/host_if.sv
 rtl/kg_fsm.sv
 
